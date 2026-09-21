524e57355930535e442571231c4773d77195040f
biddutehossen38 <biddutehossen38@gmail.com>
2026-09-21 09:55:11 +0600

    env added

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -5,9 +5,16 @@ import { IBook } from '@/types/books.type';
 
 
 const getBooks = async () => {
-  const res = await fetch('http://localhost:3000/booksData.json');
-  const data = await res.json();
-  return data;
+  try {
+
+    const res = await fetch(`${process.env.NEXT_PUBLIC_SERVER_BASE_URL}(/booksData.json)`);
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching books data:", error)
+    return [];
+  }
+
 };
 
 const Books = async () => {
